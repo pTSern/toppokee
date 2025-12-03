adb4f8470a4689f1b078a96030bd5e859af6f43b
pTSern <tsern.dev@gmail.com>
2025-12-03 22:59:50 +0700

    up

--- cocos-js/spine-9a8528df.js
+++ cocos-js/spine-9a8528df.js
@@ -1,1 +1,10 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-59f406dc.wasm")}}}));
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine = exports('default', 'assets/spine-59f406dc.wasm'); /* asset-hash:78ca8b3f */
+
+		})
+	};
+}));
