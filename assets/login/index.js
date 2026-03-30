297cbb13f3fe2a631ea6aaef52647e24e9f93dc1
pTSern <crackchuoi999@gmail.com>
2026-03-30 22:52:05 +0700

    push

--- assets/login/index.js
+++ assets/login/index.js
@@ -1,18 +1,4 @@
-System.register("chunks:///_virtual/login",["./Login.Scene.ts","./Login.UIApple.ts","./Login.UIEmail.ts","./Login.UIFacebook.ts","./Login.UIForgotPassword.ts","./Login.UIGoogle.ts","./Login.UIGuest.ts"],(function(){return{setters:[null,null,null,null,null,null,null],execute:function(){}}}));
-
-System.register("chunks:///_virtual/Login.Scene.ts",["cc"],(function(){var e;return{setters:[function(c){e=c.cclegacy}],execute:function(){e._RF.push({},"4584741e85N6624jluBXN10","Login.Scene",void 0),e._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Login.UIApple.ts",["cc"],(function(){var e;return{setters:[function(t){e=t.cclegacy}],execute:function(){e._RF.push({},"99d00bJb7dBRZr/b04xDhoq","Login.UIApple",void 0),e._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Login.UIEmail.ts",["cc"],(function(){var t;return{setters:[function(c){t=c.cclegacy}],execute:function(){t._RF.push({},"9d205ZRjBRHSpcS1tC1moMF","Login.UIEmail",void 0),t._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Login.UIFacebook.ts",["cc"],(function(){var c;return{setters:[function(e){c=e.cclegacy}],execute:function(){c._RF.push({},"ad430CZG/hOUblgZgppZBLF","Login.UIFacebook",void 0),c._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Login.UIForgotPassword.ts",["cc"],(function(){var o;return{setters:[function(t){o=t.cclegacy}],execute:function(){o._RF.push({},"efde1yHISxNx5lpsLg9EGvg","Login.UIForgotPassword",void 0),o._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Login.UIGoogle.ts",["cc"],(function(){var e;return{setters:[function(c){e=c.cclegacy}],execute:function(){e._RF.push({},"a69c9pJIqZH5YqspmM+RvBR","Login.UIGoogle",void 0),e._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Login.UIGuest.ts",["./rollupPluginModLoBabelHelpers.js","cc","./BaseUI.ts"],(function(e){var t,n,r,i,o,s,u,a;return{setters:[function(e){t=e.applyDecoratedDescriptor,n=e.inheritsLoose,r=e.initializerDefineProperty,i=e.assertThisInitialized},function(e){o=e.cclegacy,s=e._decorator,u=e.Node},function(e){a=e.BaseUI}],execute:function(){var c,l,p,f,g;o._RF.push({},"143b6IVyA5DnbqBG9HbLKDj","Login.UIGuest",void 0);var y=s.ccclass,b=s.property;e("Login_UIGuest",(c=y("Login.UIGuest"),l=b({type:u,group:a._EGroup.base}),c((g=t((f=function(e){function t(){for(var t,n=arguments.length,o=new Array(n),s=0;s<n;s++)o[s]=arguments[s];return t=e.call.apply(e,[this].concat(o))||this,r(t,"testBtn",g,i(t)),t}return n(t,e),t}(a)).prototype,"testBtn",[l],{configurable:!0,enumerable:!0,writable:!0,initializer:function(){return null}}),p=f))||p));o._RF.pop()}}}));
+System.register("chunks:///_virtual/login",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/login', 'chunks:///_virtual/login'); 
