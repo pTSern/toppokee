ab792dec979841db76522ec93ad360dd71f1ee58
pTSern <crackchuoi999@gmail.com>
2026-04-01 23:38:07 +0700

    push

--- assets/login/index.js
+++ assets/login/index.js
@@ -1,4 +1,12 @@
-System.register("chunks:///_virtual/login",[],(function(){return{execute:function(){}}}));
+System.register("chunks:///_virtual/login",["./Login.Popup.Facebook.ts","./Login.Popup.Google.ts","./Login.Popup.Guest.ts","./Login.Screen.ts"],(function(){return{setters:[null,null,null,null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/Login.Popup.Facebook.ts",["cc"],(function(){var o;return{setters:[function(e){o=e.cclegacy}],execute:function(){o._RF.push({},"6e088Cmg+5Hlrlhoup2SChy","Login.Popup.Facebook",void 0),o._RF.pop()}}}));
+
+System.register("chunks:///_virtual/Login.Popup.Google.ts",["cc"],(function(){var o;return{setters:[function(e){o=e.cclegacy}],execute:function(){o._RF.push({},"82dacUQL6dAhoU/meVE/ZGO","Login.Popup.Google",void 0),o._RF.pop()}}}));
+
+System.register("chunks:///_virtual/Login.Popup.Guest.ts",["cc"],(function(){var t;return{setters:[function(e){t=e.cclegacy}],execute:function(){t._RF.push({},"7e1f8hz0CZJzKh7/gjWDxnt","Login.Popup.Guest",void 0),t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/Login.Screen.ts",["cc"],(function(){var e;return{setters:[function(c){e=c.cclegacy}],execute:function(){e._RF.push({},"b25eeBKVz5BiJTpDOUuRxd7","Login.Screen",void 0),e._RF.pop()}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/login', 'chunks:///_virtual/login'); 
